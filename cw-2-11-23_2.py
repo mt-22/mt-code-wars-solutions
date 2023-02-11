9ee3cf5386223192b3f9bbd262e74ebe2c224508
mt-22 <39211762+mt-22@users.noreply.github.com>
2023-02-10 23:35:16 -0800

    Delete cw-2-11-23_2.py

--- cw-2-11-23_2.py
+++
@@ -1,8 +0,0 @@
-def spin_words(sentence):
-    sentence_arr = sentence.split()
-    resp = []
-    for word in sentence_arr:
-        if len(word) >= 5:
-            resp.append(word[::-1])
-        else: resp.append(word)
-    return ' '.join((x for x in resp))
