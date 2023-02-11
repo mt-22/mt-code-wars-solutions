7ae9f35e6d2697798cab4df6e1c7d6f727e8ef9a
mt-22 <39211762+mt-22@users.noreply.github.com>
2023-02-10 23:35:23 -0800

    Delete cw_2-11-23_1.py

--- cw_2-11-23_1.py
+++
@@ -1,7 +0,0 @@
-def to_camel_case(text):
-    string = ""
-    for index, letter in enumerate(text):
-        if text[index-1] == '_' or text[index-1] == '-':
-            string += letter.upper()
-        else: string += letter
-    return string.replace('_', '').replace('-', '')
